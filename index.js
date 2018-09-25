a818e98b9d5758b74645176b9616b93531e45b0a
arjun <arjunagarwal1998@gmail.com>
2018-09-25 22:32:29 +0530

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const app=express();
+require('dotenv').config();
+app.use(express.static('public'));
+
+app.listen(process.env.PORT,()=>{
+    console.log("listening");
+});
